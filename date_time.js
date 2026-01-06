1440918b5ab49f6b4b290272d5ed18acbdf26afc
pnegi5299 <pnegi5299@gmail.com>
2026-01-07 02:05:02 +0530

    js advance

---
+++ date_time.js
@@ -0,0 +1,25 @@
+//? new Date
+// const currentDate = new Date()
+// console.log(currentDate);
+
+//? new Date(dateString)
+// const dateString= '2026-01-02T14:26:14.283Z'
+// const dateFromString = new Date(dateString)
+// console.log(dateFromString);
+
+//? new Date(year, month)
+// const date1= new Date(2026,1)
+// console.log(date1);
+
+//? new Date(year, month ,day)
+// const date2= new Date(2026,0,2);
+// console.log(date2);
+
+
+const curMilliseconds= new Date().getTime()
+console.log(curMilliseconds);
+
+
+
+
+
---
+++ local.js
@@ -0,0 +1,7 @@
+localStorage.setItem("jsCourseTest","addingData"); //ker : value
+
+localStorage.getItem("jsCourseTest")
+
+localStorage.removeItem("jsCoursetest")
+
+localStorage.setItem("['apple','mango','grapes']");
