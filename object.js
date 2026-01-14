72cd96c5e8094f99a1ed16273810fd4412832a2d
pnegi5299 <pnegi5299@gmail.com>
2026-01-14 22:29:59 +0530

    feat new lines  added

--- object.js
+++ object.js
@@ -21,19 +21,42 @@ const person ={
 // person.age=18;
 // console.log(person);
 
-let idType="StudentID"
-let student={
-    [idType]:'A123456',
-    sName:'priyanshu',
-    sAge:22,
-    isStudent:true,
-    greet:function(){
-        console.log(`Hey, my ${idType} is ${student[idType]} and my name is ${student.sName}.`);
+// let idType="StudentID"
+// let student={
+//     [idType]:'A123456',
+//     sName:'priyanshu',
+//     sAge:22,
+//     isStudent:true,
+//     greet:function(){
+//         console.log(`Hey, my ${idType} is ${student[idType]} and my name is ${student.sName}.`);
         
+//     }
+// };
+
+// student.greet()
+
+
+const areObjectsEqual = (obj1, obj2) => {
+    let o1 = Object.keys(obj1);
+    let o2 = Object.keys(obj2);
+
+    if (o1.length != o2.length) {
+        console.log('These keys are not same');
+        return false;
     }
+    for (let key in obj1) {
+        if (obj1[key] != obj2[key]) {
+            return false;
+        }
+    }
+    return true;
 };
 
-student.greet()
+let obja = {name:'alice', age:26, city:'new pork'};
+let objb = {name:'alice', age:26, city:'new york'};
+
+console.log(areObjectsEqual(obja, objb));
+
 
 
 
